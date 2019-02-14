326e494b15efece83da4bc519aae33cb132c4477
Chamod Priyamal <chamod.priyamal@apweslk.com>
2019-02-14 14:32:39 +0530

    Errors Fixed

--- TechnoVision/TechnoVision/view/UI_LOGIN.Designer.cs
+++ TechnoVision/TechnoVision/view/UI_LOGIN.Designer.cs
@@ -43,64 +43,64 @@
             // 
             // 
             // 
-            this.UserName.CustomButton.Image = null;
-            this.UserName.CustomButton.Location = new System.Drawing.Point(185, 2);
-            this.UserName.CustomButton.Name = "";
-            this.UserName.CustomButton.Size = new System.Drawing.Size(25, 25);
-            this.UserName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.UserName.CustomButton.TabIndex = 1;
-            this.UserName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.UserName.CustomButton.UseSelectable = true;
-            this.UserName.CustomButton.Visible = false;
-            this.UserName.FontSize = MetroFramework.MetroTextBoxSize.Tall;
-            this.UserName.Lines = new string[0];
-            this.UserName.Location = new System.Drawing.Point(183, 145);
-            this.UserName.MaxLength = 32767;
-            this.UserName.Name = "UserName";
-            this.UserName.PasswordChar = '\0';
-            this.UserName.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.UserName.SelectedText = "";
-            this.UserName.SelectionLength = 0;
-            this.UserName.SelectionStart = 0;
-            this.UserName.ShortcutsEnabled = true;
-            this.UserName.Size = new System.Drawing.Size(213, 30);
-            this.UserName.TabIndex = 0;
-            this.UserName.UseSelectable = true;
-            this.UserName.WaterMark = "Enter Username";
-            this.UserName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.UserName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.TxtUsername.CustomButton.Image = null;
+            this.TxtUsername.CustomButton.Location = new System.Drawing.Point(185, 2);
+            this.TxtUsername.CustomButton.Name = "";
+            this.TxtUsername.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.TxtUsername.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtUsername.CustomButton.TabIndex = 1;
+            this.TxtUsername.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtUsername.CustomButton.UseSelectable = true;
+            this.TxtUsername.CustomButton.Visible = false;
+            this.TxtUsername.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.TxtUsername.Lines = new string[0];
+            this.TxtUsername.Location = new System.Drawing.Point(183, 145);
+            this.TxtUsername.MaxLength = 32767;
+            this.TxtUsername.Name = "UserName";
+            this.TxtUsername.PasswordChar = '\0';
+            this.TxtUsername.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtUsername.SelectedText = "";
+            this.TxtUsername.SelectionLength = 0;
+            this.TxtUsername.SelectionStart = 0;
+            this.TxtUsername.ShortcutsEnabled = true;
+            this.TxtUsername.Size = new System.Drawing.Size(213, 30);
+            this.TxtUsername.TabIndex = 0;
+            this.TxtUsername.UseSelectable = true;
+            this.TxtUsername.WaterMark = "Enter Username";
+            this.TxtUsername.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtUsername.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
             // Password
             // 
             // 
             // 
             // 
-            this.Password.CustomButton.Image = null;
-            this.Password.CustomButton.Location = new System.Drawing.Point(185, 2);
-            this.Password.CustomButton.Name = "";
-            this.Password.CustomButton.Size = new System.Drawing.Size(25, 25);
-            this.Password.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.Password.CustomButton.TabIndex = 1;
-            this.Password.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.Password.CustomButton.UseSelectable = true;
-            this.Password.CustomButton.Visible = false;
-            this.Password.FontSize = MetroFramework.MetroTextBoxSize.Tall;
-            this.Password.Lines = new string[0];
-            this.Password.Location = new System.Drawing.Point(183, 181);
-            this.Password.MaxLength = 32767;
-            this.Password.Name = "Password";
-            this.Password.PasswordChar = '*';
-            this.Password.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.Password.SelectedText = "";
-            this.Password.SelectionLength = 0;
-            this.Password.SelectionStart = 0;
-            this.Password.ShortcutsEnabled = true;
-            this.Password.Size = new System.Drawing.Size(213, 30);
-            this.Password.TabIndex = 1;
-            this.Password.UseSelectable = true;
-            this.Password.WaterMark = "Enter Password";
-            this.Password.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.Password.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.TxtPassword.CustomButton.Image = null;
+            this.TxtPassword.CustomButton.Location = new System.Drawing.Point(185, 2);
+            this.TxtPassword.CustomButton.Name = "";
+            this.TxtPassword.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.TxtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtPassword.CustomButton.TabIndex = 1;
+            this.TxtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtPassword.CustomButton.UseSelectable = true;
+            this.TxtPassword.CustomButton.Visible = false;
+            this.TxtPassword.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.TxtPassword.Lines = new string[0];
+            this.TxtPassword.Location = new System.Drawing.Point(183, 181);
+            this.TxtPassword.MaxLength = 32767;
+            this.TxtPassword.Name = "Password";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtPassword.SelectedText = "";
+            this.TxtPassword.SelectionLength = 0;
+            this.TxtPassword.SelectionStart = 0;
+            this.TxtPassword.ShortcutsEnabled = true;
+            this.TxtPassword.Size = new System.Drawing.Size(213, 30);
+            this.TxtPassword.TabIndex = 1;
+            this.TxtPassword.UseSelectable = true;
+            this.TxtPassword.WaterMark = "Enter Password";
+            this.TxtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
             // BtnLogin
             // 
