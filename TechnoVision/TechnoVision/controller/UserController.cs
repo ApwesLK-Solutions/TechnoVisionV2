940378e82a335041b6a0e29274ae31fac59d1a61
Chamod Priyamal <chamod.priyamal@apweslk.com>
2019-02-14 16:22:43 +0530

    forget password is done

--- TechnoVision/TechnoVision/controller/UserController.cs
+++ TechnoVision/TechnoVision/controller/UserController.cs
@@ -17,7 +17,7 @@ namespace TechnoVision.controller
         public static void login(MetroForm form)
         {
             try
-            {                
+            {
                 row = userTable.FindByUsername(User.Username);
                 if (row == null)
                 {
@@ -25,26 +25,26 @@ namespace TechnoVision.controller
 
                     // MetroFramework.MetroMessageBox.Show(form, row.Password);
                     Console.WriteLine("user not found");
-                    CommonFunctions.WriteUserLog("SYSTEM","Failed Login Attemp With Username -> " + User.Username);
+                    CommonFunctions.WriteUserLog("SYSTEM", "Failed Login Attemp With Username -> " + User.Username);
                     //return false;
-                    
+
                 }
                 else if ((row != null) && (User.Password != row.Password))
                 {
                     //wrong password      
-                    Console.WriteLine("wrong password");         
-                    CommonFunctions.WriteUserLog("SYSTEM",User.Username + " Tried to Login with Wrong Password");
+                    Console.WriteLine("wrong password");
+                    CommonFunctions.WriteUserLog("SYSTEM", User.Username + " Tried to Login with Wrong Password");
                     //return false;
                 }
-                else if((User.Username == row.Username) && (row.Password == User.Password) && (row.IsActive == 0))
+                else if ((User.Username == row.Username) && (row.Password == User.Password) && (row.IsActive == 0))
                 {
                     //Disabled User
                     Console.WriteLine("Deleted user");
-                    CommonFunctions.WriteUserLog("SYSTEM","A Disabled User Tried to Login > "+User.Username);
+                    CommonFunctions.WriteUserLog("SYSTEM", "A Disabled User Tried to Login > " + User.Username);
                     //return false;
                 }
 
-                else if((User.Username == row.Username) && (row.Password == User.Password))
+                else if ((User.Username == row.Username) && (row.Password == User.Password))
                 {
                     //login success
                     Console.WriteLine("login success");
@@ -52,16 +52,16 @@ namespace TechnoVision.controller
                     Session.BranchId = row.BranchId;
                     Brow = branchTable.FindById(row.BranchId);
                     Session.BranchMasterPassword = Brow.MasterPassword;
-                    CommonFunctions.WriteUserLog("SYSTEM" , User.Username + " is Log into the System");
+                    CommonFunctions.WriteUserLog("SYSTEM", User.Username + " is Log into the System");
                     //return true;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 //show some error and log the original error!
                 CommonFunctions.WriteToErrorLog(ex.Message.ToString());
                 //return false;
-            }         
+            }
         }
         public static void RegisterNewUser(MetroForm form)
         {
@@ -71,7 +71,7 @@ namespace TechnoVision.controller
                 usersAdapter.Insert(User.Username, User.Password, User.FullName, User.Nic, User.ContactNumber, User.BranchId, 1);
                 CommonFunctions.WriteUserLog(Session.Username, "Added New User to the System (" + User.Username + ")");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 //error
                 CommonFunctions.WriteToErrorLog(ex.Message.ToString());
@@ -103,5 +103,33 @@ namespace TechnoVision.controller
             }
         }
 
+        public static void UpdatePassword(MetroForm form, string username, string password, string confirmPassword, string Contact, string nic)
+        {
+            try
+            {
+                row = userTable.FindByUsername(username);
+                if (row == null)
+                {
+                    //user not exists
+                }
+                else if (password != confirmPassword)
+                {
+                    //pasword not match
+                }
+                else if ((Contact != row.ContactNumber) || (nic != row.Nic))
+                {
+                    //cant verify
+                }
+                else
+                {
+                    usersAdapter.UpdateUserPasswordByUsername(password, username);
+                    CommonFunctions.WriteUserLog(Session.Username, "Changed the Password of " + username);
+                }
+            }
+            catch(Exception ex)
+            {
+                CommonFunctions.WriteToErrorLog(ex.Message);
+            }
+        }
     }
 }
