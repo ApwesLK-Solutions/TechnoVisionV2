b735a9f912f61693d540bb173f11161710860ec5
shehantha Edirisinghe <37338677+shehantha08@users.noreply.github.com>
2019-02-14 12:31:07 +0530

    add system user done

--- TechnoVision/TechnoVision/Program.cs
+++ TechnoVision/TechnoVision/Program.cs
@@ -17,7 +17,7 @@ namespace TechnoVision
             if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UI_LOGIN());
+            Application.Run(new UI_ADD_USER());
         }
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
