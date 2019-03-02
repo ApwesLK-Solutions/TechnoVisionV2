2e36ffc252fa085fa631c01c7ba5494eb0add587
shehantha Edirisinghe <37338677+shehantha08@users.noreply.github.com>
2019-03-02 16:11:25 +0530

    saa

---
+++ TechnoVision/TechnoVision/view/UI_ORDER_LIST.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TechnoVision.view
+{
+    partial class UI_ORDER_LIST
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnViewCompanyCopy = new MetroFramework.Controls.MetroButton();
+            this.BtnViewCustomerCopy = new MetroFramework.Controls.MetroButton();
+            this.CmbSetAs = new MetroFramework.Controls.MetroComboBox();
+            this.BtnMarkAs = new MetroFramework.Controls.MetroButton();
+            this.RadioAll = new MetroFramework.Controls.MetroRadioButton();
+            this.RadioContactLense = new MetroFramework.Controls.MetroRadioButton();
+            this.RadioSpectacles = new MetroFramework.Controls.MetroRadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnViewCompanyCopy
+            // 
+            this.BtnViewCompanyCopy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnViewCompanyCopy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnViewCompanyCopy.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.BtnViewCompanyCopy.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnViewCompanyCopy.Highlight = true;
+            this.BtnViewCompanyCopy.Location = new System.Drawing.Point(970, 594);
+            this.BtnViewCompanyCopy.Name = "BtnViewCompanyCopy";
+            this.BtnViewCompanyCopy.Size = new System.Drawing.Size(162, 45);
+            this.BtnViewCompanyCopy.Style = MetroFramework.MetroColorStyle.Green;
+            this.BtnViewCompanyCopy.TabIndex = 5;
+            this.BtnViewCompanyCopy.Text = "View Company Copy";
+            this.BtnViewCompanyCopy.UseCustomBackColor = true;
+            this.BtnViewCompanyCopy.UseCustomForeColor = true;
+            this.BtnViewCompanyCopy.UseSelectable = true;
+            this.BtnViewCompanyCopy.UseStyleColors = true;
+            // 
+            // BtnViewCustomerCopy
+            // 
+            this.BtnViewCustomerCopy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnViewCustomerCopy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnViewCustomerCopy.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.BtnViewCustomerCopy.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnViewCustomerCopy.Highlight = true;
+            this.BtnViewCustomerCopy.Location = new System.Drawing.Point(812, 594);
+            this.BtnViewCustomerCopy.Name = "BtnViewCustomerCopy";
+            this.BtnViewCustomerCopy.Size = new System.Drawing.Size(152, 45);
+            this.BtnViewCustomerCopy.Style = MetroFramework.MetroColorStyle.Green;
+            this.BtnViewCustomerCopy.TabIndex = 4;
+            this.BtnViewCustomerCopy.Text = "View Customer Copy";
+            this.BtnViewCustomerCopy.UseCustomBackColor = true;
+            this.BtnViewCustomerCopy.UseCustomForeColor = true;
+            this.BtnViewCustomerCopy.UseSelectable = true;
+            this.BtnViewCustomerCopy.UseStyleColors = true;
+            // 
+            // CmbSetAs
+            // 
+            this.CmbSetAs.FontSize = MetroFramework.MetroComboBoxSize.Tall;
+            this.CmbSetAs.FormattingEnabled = true;
+            this.CmbSetAs.ItemHeight = 29;
+            this.CmbSetAs.Items.AddRange(new object[] {
+            "DELEVERED",
+            "CANCELED"});
+            this.CmbSetAs.Location = new System.Drawing.Point(19, 599);
+            this.CmbSetAs.Name = "CmbSetAs";
+            this.CmbSetAs.Size = new System.Drawing.Size(228, 35);
+            this.CmbSetAs.TabIndex = 2;
+            this.CmbSetAs.UseSelectable = true;
+            // 
+            // BtnMarkAs
+            // 
+            this.BtnMarkAs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnMarkAs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMarkAs.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.BtnMarkAs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnMarkAs.Highlight = true;
+            this.BtnMarkAs.Location = new System.Drawing.Point(253, 594);
+            this.BtnMarkAs.Name = "BtnMarkAs";
+            this.BtnMarkAs.Size = new System.Drawing.Size(152, 45);
+            this.BtnMarkAs.Style = MetroFramework.MetroColorStyle.Green;
+            this.BtnMarkAs.TabIndex = 3;
+            this.BtnMarkAs.Text = "Mark As";
+            this.BtnMarkAs.UseCustomBackColor = true;
+            this.BtnMarkAs.UseCustomForeColor = true;
+            this.BtnMarkAs.UseSelectable = true;
+            this.BtnMarkAs.UseStyleColors = true;
+            // 
+            // RadioAll
+            // 
+            this.RadioAll.AutoSize = true;
+            this.RadioAll.Checked = true;
+            this.RadioAll.FontSize = MetroFramework.MetroCheckBoxSize.Tall;
+            this.RadioAll.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold;
+            this.RadioAll.Location = new System.Drawing.Point(354, 21);
+            this.RadioAll.Name = "RadioAll";
+            this.RadioAll.Size = new System.Drawing.Size(51, 25);
+            this.RadioAll.Style = MetroFramework.MetroColorStyle.Green;
+            this.RadioAll.TabIndex = 6;
+            this.RadioAll.Text = "All";
+            this.RadioAll.UseSelectable = true;
+           
+            // 
+            // RadioContactLense
+            // 
+            this.RadioContactLense.AutoSize = true;
+            this.RadioContactLense.FontSize = MetroFramework.MetroCheckBoxSize.Tall;
+            this.RadioContactLense.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold;
+            this.RadioContactLense.Location = new System.Drawing.Point(609, 21);
+            this.RadioContactLense.Name = "RadioContactLense";
+            this.RadioContactLense.Size = new System.Drawing.Size(160, 25);
+            this.RadioContactLense.Style = MetroFramework.MetroColorStyle.Green;
+            this.RadioContactLense.TabIndex = 6;
+            this.RadioContactLense.Text = "Contact Lenses";
+            this.RadioContactLense.UseSelectable = true;
+            // 
+            // RadioSpectacles
+            // 
+            this.RadioSpectacles.AutoSize = true;
+            this.RadioSpectacles.FontSize = MetroFramework.MetroCheckBoxSize.Tall;
+            this.RadioSpectacles.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold;
+            this.RadioSpectacles.Location = new System.Drawing.Point(444, 20);
+            this.RadioSpectacles.Name = "RadioSpectacles";
+            this.RadioSpectacles.Size = new System.Drawing.Size(119, 25);
+            this.RadioSpectacles.Style = MetroFramework.MetroColorStyle.Green;
+            this.RadioSpectacles.TabIndex = 6;
+            this.RadioSpectacles.Text = "Spectacles";
+            this.RadioSpectacles.UseSelectable = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RadioContactLense);
+            this.groupBox1.Controls.Add(this.RadioSpectacles);
+            this.groupBox1.Controls.Add(this.RadioAll);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1109, 56);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Searching Criteria";
+            // 
+            // UI_ORDER_LIST
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 650);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.CmbSetAs);
+            this.Controls.Add(this.BtnViewCompanyCopy);
+            this.Controls.Add(this.BtnMarkAs);
+            this.Controls.Add(this.BtnViewCustomerCopy);
+            this.MaximizeBox = false;
+            this.Name = "UI_ORDER_LIST";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Order List";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton BtnViewCompanyCopy;
+        private MetroFramework.Controls.MetroButton BtnViewCustomerCopy;
+        private MetroFramework.Controls.MetroComboBox CmbSetAs;
+        private MetroFramework.Controls.MetroButton BtnMarkAs;
+        private MetroFramework.Controls.MetroRadioButton RadioAll;
+        private MetroFramework.Controls.MetroRadioButton RadioContactLense;
+        private MetroFramework.Controls.MetroRadioButton RadioSpectacles;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
