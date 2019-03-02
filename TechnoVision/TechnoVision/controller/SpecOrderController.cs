fbc63a22474dbfdebecd58c97d5c43e89dc1c745
Chamod Priyamal <chamod.priyamal@apweslk.com>
2019-03-02 09:07:04 +0530

    fix

--- TechnoVision/TechnoVision/controller/SpecOrderController.cs
+++ TechnoVision/TechnoVision/controller/SpecOrderController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TechnoVision.model;
 namespace TechnoVision.controller
 {
     class SpecOrderController
