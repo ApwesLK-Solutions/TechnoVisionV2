8ea26d816e46931b0c9c810d918b265d98d32a64
shehantha Edirisinghe <37338677+shehantha08@users.noreply.github.com>
2019-03-02 14:16:07 +0530

    test and frame

--- TechnoVision/TechnoVision/view/UI_MANAGE_TESTERS.cs
+++ TechnoVision/TechnoVision/view/UI_MANAGE_TESTERS.cs
@@ -19,23 +19,57 @@ namespace TechnoVision.view
 
         private void UI_MANAGE_TESTERS_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'technovisionDataSet.testers' table. You can move, or remove it, as needed.
-            this.testersTableAdapter.Fill(this.technovisionDataSet.testers);
-
+            try
+            {
+                // TODO: This line of code loads data into the 'technovisionDataSet.testers' table. You can move, or remove it, as needed.
+                this.testersTableAdapter.Fill(this.technovisionDataSet.testers);
+            }
+            catch(Exception ex)
+            {
+                CommonFunctions.WriteToErrorLog(ex.Message.ToString());
+                CommonFunctions.ShowError(this, "Can not Load Testers...");
+            }
         }
 
         private void BtnAddTester_Click(object sender, EventArgs e)
         {
-            technovisionDataSetTableAdapters.testersTableAdapter t = new technovisionDataSetTableAdapters.testersTableAdapter();
-            t.Insert(TxtAddTester.Text);
-            this.testersTableAdapter.Fill(this.technovisionDataSet.testers);
+            try
+            {
+                if(TxtAddTester.Text =="")
+                {
+                    CommonFunctions.ShowError(this, "Please Enter Tester Name...");
+                }
+                else
+                {
+                    technovisionDataSetTableAdapters.testersTableAdapter t = new technovisionDataSetTableAdapters.testersTableAdapter();
+                    t.Insert(TxtAddTester.Text);
+                    this.testersTableAdapter.Fill(this.technovisionDataSet.testers);
+                    CommonFunctions.ShowSuccess(this, "New Tester Saved Successfully...");
+                    CommonFunctions.WriteUserLog(Session.Username, "Added new Tester" + TxtAddTester.Text);
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                CommonFunctions.WriteToErrorLog(ex.Message.ToString());
+            }
+            
         }
 
         private void BtnDeleteTester_Click(object sender, EventArgs e)
         {
-            technovisionDataSetTableAdapters.testersTableAdapter t = new technovisionDataSetTableAdapters.testersTableAdapter();
-            t.DeleteById(int.Parse(GridTesters.SelectedRows[0].Cells[0].Value.ToString()));
-            this.testersTableAdapter.Fill(this.technovisionDataSet.testers);
+            try
+            {
+                technovisionDataSetTableAdapters.testersTableAdapter t = new technovisionDataSetTableAdapters.testersTableAdapter();
+                t.DeleteById(int.Parse(GridTesters.SelectedRows[0].Cells[0].Value.ToString()));
+                this.testersTableAdapter.Fill(this.technovisionDataSet.testers);
+            }
+            catch(Exception ex)
+            {
+                CommonFunctions.ShowError(this, "Nothing To Delete Or Not Selected...");
+                CommonFunctions.WriteToErrorLog(ex.Message.ToString());
+            }
+            
 
         }
     }
