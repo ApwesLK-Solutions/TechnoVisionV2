dbaf608c795619c1aef81073f99344f2fff4494b
Chamod Priyamal <chamod.priyamal@apweslk.com>
2019-03-02 16:13:06 +0530

    report viewer

---
+++ TechnoVision/TechnoVision/view/UI_REPORT_VIEWER.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace TechnoVision.view
+{
+    public partial class UI_REPORT_VIEWER : MetroFramework.Forms.MetroForm
+    {
+        public UI_REPORT_VIEWER()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_REPORT_VIEWER_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
